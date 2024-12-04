7aa6f8c10569765cc6cef5dd224f656fa279cb85
aaminha <dks4857@gmail.com>
2024-12-04 16:48:21 +0900

    lec: 노맏드 코더 강의 - Movie App part 2 & React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,12 @@
-import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
-interface Movie {
-  id: number;
-  title: string;
-  summary: string;
-  genres: string[];
-  medium_cover_image: string;
-}
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState<Movie[]>([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
   return (
-    <div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt={movie.title} />
-              <h2 className="text-2xl font-bold">{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul className="list-inside list-disc">
-                {movie.genres.map((genre) => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<Detail />} />
+    </Routes>
   );
 }
